763cee47186d9829cc30c5935219f5a4b02437d8
Masaki Komagata <komagata@gmail.com>
2017-12-21 20:24:14 +0900

    ゲームタイマーを追加

---
+++ Assets/Scripts/GameTimerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerController : MonoBehaviour {
+	public Image CircleImage;
+	public float countTime = 60.0f;
+
+	void Update () {
+		CircleImage.fillAmount -= 1.0f / countTime * Time.deltaTime;
+	}
+}
